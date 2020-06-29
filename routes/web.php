00cc0cb8abb27eae726a6ccdf92290bf9e398fbb
Backend Server <backendserver@gmail.com>
2020-06-29 14:54:30 +0700

    membuat component alert and aktifkan fitur auth

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::name('admin.')
+    ->prefix('admin')
+    ->namespace('Admin')
+    ->middleware(['auth', 'role:superadmin'])
+    ->group(function () {
+        Route::resource('user', 'UserController');
+        Route::resource('permission', 'PermissionController');
+        Route::resource('role', 'RoleController');
+    });
+
+Route::resource('article', 'ArticleController');
